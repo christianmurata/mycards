8e1ff8834d60ce58f1033fe61b165dfd3e6f05ac
Christian Murata <chris_murata@hotmail.com>
2022-08-04 20:02:34 -0300

    feat: subscriptions

--- src/resolvers/cardResolvers.ts
+++ src/resolvers/cardResolvers.ts
@@ -1,27 +1,48 @@
+import { PubSub } from 'graphql-subscriptions';
+
 import { Resolvers } from "../../generated/graphql";
 import { Card } from "../models/Card";
 
+export const pubsub = new PubSub();
+
 export const cardResolvers: Resolvers = {
   Query: {
-    card: async (_, { id }) => {
-      return await Card.findById(id);
-    },
-    cards: async (_, { }) => {
-      return await Card.find({});
-    },
-    cardsByType: async (_, { type }) => {
-      return await Card.find({ type });
-    },
+    card: async (_, { id }) => await Card.findById(id),
+    cards: async (_, { }) => await Card.find({}),
+    cardsByType: async (_, { type }) => await Card.find({ type }),
   },
   Mutation: {
     createCard: async (_, { card }) => {
-      return await (new Card(card)).save();
+      const newCard = await (new Card(card)).save();
+      pubsub.publish('CARD_CREATED', { cardCreated: newCard }); 
+
+      return card;
     },
     updateCard: async (_, { id, card }) => {
-      return await Card.findOneAndUpdate({ _id: id }, card, { new: true });
+      const newCard = await Card.findOneAndUpdate({ _id: id }, card, { new: true });
+      pubsub.publish('CARD_UPDATED', { cardUpdated: newCard }); 
+
+      return newCard;
     },
     deleteCard: async (_, { id }) => {
-      return await Card.findByIdAndDelete(id);
+      const card = await Card.findByIdAndDelete(id);
+      pubsub.publish('CARD_DELETED', { cardDeleted: card }); 
+
+      return card;
     },
-  }
+  },
+  Subscription: {
+    cardCreated: {
+      // @ts-ignore
+      subscribe: () => pubsub.asyncIterator('CARD_CREATED'),
+    },
+    cardUpdated: {
+      // @ts-ignore
+      subscribe: () => pubsub.asyncIterator('CARD_UPDATED'),
+    },
+    cardDeleted: {
+      // @ts-ignore
+      subscribe: () => pubsub.asyncIterator('CARD_DELETED'),
+    },
+  },
 };
